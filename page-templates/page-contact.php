b21314b288ac82c54c543ca05fbf3287a1078008
Danylo <antoni@everywhere.pl>
2022-05-11 14:40:53 +0200

    Badanie Kliniczne

--- page-templates/page-contact.php
+++ page-templates/page-contact.php
@@ -3,7 +3,7 @@
 get_header();
 get_template_part('template-parts/components/component-breadcrumb', null, array('post_id'=>$post->ID));
 ?>
-<h1 class="title-subtitle__title fw-normal text-center mb-5">Skontaktuj się z nami</h1>
+<h1 class="title-subtitle__title fw-normal text-center mb-5 pb-5">Skontaktuj się z nami</h1>
 <?php
 get_template_part('template-parts/contact/contact-people');
 get_template_part('template-parts/contact/contact-map-section');
